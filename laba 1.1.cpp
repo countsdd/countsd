66ddde8f933c0735bedae08797a286c24f37a096
countsdd <ermilova10@icloud.com>
2025-09-26 01:08:32 +0200

    Add files via upload

---
+++ laba 1.1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int x; // 4 байта
+    // выбираем int, потому что x число целое
+
+    std::cout << "x: " << std::endl;
+    std::cin >> x; // ввод x с клавиатуры
+
+    float x_rec= 1/(float)x; // находим обратное числу x, 4 байта, 6-7 значащих чисел
+    // выбираем fload, потому что значение 1/x получится число дробное с плавающей точкой
+    
+    int x_2 = std::pow(x,2);  // возведение x в 2 степень
+    // выбираем int, потому что значение x_2 получится число целое
+    
+        int x_5 = std::pow(x, 5); // возведение x в 5 степень
+    // выбираем int, потому что значение x_5 получится число целое
+
+    // вывод результатов 
+    std::cout << "x_rec= " << x_rec << std::endl;
+    std::cout << "x_2= "<< x_2 << std::endl;
+    std::cout << "x_5= " << x_5 << std::endl;
+
+    std::cout << "int: занимает "<< sizeof(int) * 8 << " бит, мин. значение = " << std::numeric_limits<int>::min() << 
+                " макс. значение = " <<std::numeric_limits<int>::max() << std::endl;
+    std::cout << "float: занимает " << sizeof(float) * 8 << " бит, мин. значение = " 
+    << std::numeric_limits<float>::min() << " макс. значение = " << std::numeric_limits<float>::max() << std::endl;
+}
+v
